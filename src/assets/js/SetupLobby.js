e08b77754ee3f9382d27f871da5320082a670c06
stijn <stijn.van.der.herten@student.howest.be>
2020-03-19 15:19:44 +0100

    Editing setup files for creating lobby --> pair programming with robin

--- src/assets/js/SetupLobby.js
+++ src/assets/js/SetupLobby.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', init);
 let __gameId = localStorage.getItem('gameID');
 let __PlayerName = localStorage.getItem('playerName');
+let __playerToken = __gameId + '+' + __PlayerName;
 
 function init() {
 
@@ -10,35 +11,36 @@ function init() {
         if (tag.innerHTML === "Start") {
             tag.addEventListener('click', loadGame);
 
-        }else if(tag.innerHTML === 'Ready'){
+        } else if (tag.innerHTML === 'Ready') {
             tag.addEventListener('click', readyUp);
 
         }
     });
     loadLobby();
-
 }
 
 function loadGame() {
+
     window.location.href = "PlayBoard.html"
 }
 
 function loadLobby() {
     let players;
-    let playerToken = __gameId + '+' + __PlayerName;
-
+    let table = document.querySelector(".center");
     fetchFromServer(`${config.root}games/${__gameId}`, 'GET')
-        .then(function(response) {
+        .then(function (response) {
             players = response['players'];
+            table.innerHTML = '';
 
-            let table = document.querySelector(".center");
             console.log(players);
+
             players.forEach(player => {
                 table.innerHTML += `<tr id="${player}">
                                 <td>${player}</td>
                                 <td>Not ready</td>
                             </tr>`;
             });
+            document.querySelector('#playerCount').innerHTML = players.length + "/6";
         });
 }
 
@@ -46,36 +48,41 @@ function readyUp() {
     let table = document.querySelectorAll(".center tr");
     let button = document.querySelector('#readyButton');
 
-    if (button.innerHTML === 'Ready'){
+    if (button.innerHTML === 'Ready') {
+        readyUpPlayer();
+        table.forEach(tag => {
+            if (tag.id === __PlayerName) {
+                tag.querySelectorAll('td').forEach(td => {
+                    if (td.innerHTML !== __PlayerName) {
+                        td.innerHTML = 'Ready'
+                    }
+                })
+            }
+        })
+    } else {
+        unReadyPlayer();
+        table.forEach(tag => {
+            if (tag.id === __PlayerName) {
+                tag.querySelectorAll('td').forEach(td => {
+                    if (td.innerHTML !== __PlayerName) {
+                        td.innerHTML = 'Not ready'
+                    }
+                })
+            }
+        })
+    }
+
+    function readyUpPlayer() {
         fetchFromServer(`${config.root}games/${__gameId}/players/${__PlayerName}/ready`, `PUT`)
             .then(function () {
                 button.innerHTML = 'Not ready';
-                table.forEach(tag => {
-                    if (tag.id === __PlayerName){
-                        tag.querySelectorAll('td').forEach(td => {
-                            if (td.innerHTML !== __PlayerName){
-                                td.innerHTML = 'Ready'
-                            }
-                        })
-                    }
-                })
-            });
+            })
+    }
 
-    }else{
+    function unReadyPlayer() {
         fetchFromServer(`${config.root}games/${__gameId}/players/${__PlayerName}/ready`, `DELETE`)
             .then(function () {
                 button.innerHTML = 'Ready';
-                table.forEach(tag => {
-                    if (tag.id === __PlayerName){
-                        tag.querySelectorAll('td').forEach(td => {
-                            if (td.innerHTML !== __PlayerName){
-                                td.innerHTML = 'Not ready'
-                            }
-                        })
-                    }
-                })
-            });
+            })
     }
-
-
 }
--- src/assets/js/communication.js
+++ src/assets/js/communication.js
@@ -7,7 +7,7 @@ function fetchFromServer(url, httpVerb, requestBody){
     options.headers = {};
     options.headers["Content-Type"] = "application/json";
 
-    options.headers["Authorization"] ="Bearer " + 'group01-021+ellie';
+    options.headers["Authorization"] ="Bearer " + __playerToken;
 
     // Don't forget to add data to the body when needed
     options.body = JSON.stringify(requestBody);
