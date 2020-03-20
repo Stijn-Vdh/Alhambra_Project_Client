2948cb57a7453a66d6e0b7b94241a889fbf1ef4f
stav.heuvinck <stav.heuvinck@student.howest.be>
2020-03-20 12:13:29 +0100

    Load bank amount and color from server. (Pair work Stav & Cirdan)

--- src/assets/js/playBoard.js
+++ src/assets/js/playBoard.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     setMyAvatar();
     getGameDetails();
+    getBankCards();
+    document.querySelector('#MoneyStacks').addEventListener('click', addMoneyCardToOwnStack)
 }
 
 function getGameDetails() {
@@ -19,6 +21,14 @@ function setMyAvatar() {
     avatar.innerHTML = `<img src="${localStorage.getItem("avatar")}" alt="myAvatar">`;
 }
 
+function setOwnStartingCoins(response) {
+    let coins = getStartingCoins(response);
+    let hand = document.querySelector("#Hand");
+    coins.forEach(coin => {
+        hand.innerHTML += `<div class="card ${coin["currency"]}"><p>${coin["amount"]}</p></div>`
+    });
+}
+
 function getStartingCoins(response) {
     let playerDetails = response["players"];
     for (let i = 0; i < playerDetails.length; i++) {
@@ -29,10 +39,35 @@ function getStartingCoins(response) {
 
 }
 
-function setOwnStartingCoins(response) {
-    let coins = getStartingCoins(response);
-    let hand = document.querySelector("#Hand");
-    coins.forEach(coin => {
-        hand.innerHTML += `<div class="card" id="${coin["currency"]}"><p>${coin["amount"]}</p></div>`
-    });
+
+function addMoneyCardToOwnStack(e){
+   console.log(e.target.closest('div').id);
+   
+    /*fetchFromServer(`${config.root}games/${localStorage.getItem("gameID")}/players/${localStorage.getItem("playerName")}`, 'GET')
+        .then(function (response) {
+            test = response;
+        });*/
+    fetchFromServer(`${config.root}games/${localStorage.getItem("gameID")}`, 'GET')
+        .then(function (response) {
+            console.log(response["bank"].length);
+        });
+
+
+}
+
+function getBankCards(){
+
+    let bank = document.querySelector("#MoneyStacks");
+
+    let counter = 1;
+
+    fetchFromServer(`${config.root}games/${localStorage.getItem("gameID")}`, 'GET')
+        .then(function (response) {
+            for (let i = 0; i < response["bank"].length; i++) {
+                    bank.innerHTML += `<div class="${response["bank"][i]["currency"]}" id="MoneyStack${counter}"><p>${response["bank"][i]["amount"]}</p></div>`;
+                    counter++;
+            }
+        });
+
+
 }
