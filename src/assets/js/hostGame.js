b486395c4de451f43edb7b182a31d930fec7f2cb
stijn <stijn.van.der.herten@student.howest.be>
2020-03-20 09:43:31 +0100

    Adding previous and current page functions

--- src/assets/js/hostGame.js
+++ src/assets/js/hostGame.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded',init);
 
 function init() {
     document.querySelector('#hostGameForm').addEventListener('submit', createNewGame);
+    setPreviousPage(localStorage.getItem('currentPage'));
+    setCurrentPage();
 }
 function createNewGame(e){
     e.preventDefault();
@@ -31,5 +33,12 @@ function createNewGame(e){
 
         });
 
+}
+
+function setPreviousPage(page) {
+    localStorage.setItem('previousPage', page)
+}
+function setCurrentPage() {
+    localStorage.setItem('CurrentPage', 'lobby.html')
 }
 
