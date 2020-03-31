b5e62631757b4fac65821c931538bfc5ec469401
stijn <stijn.van.der.herten@student.howest.be>
2020-03-31 13:52:19 +0200

    Added functions: CheckIllegal card selection & to process the selected card (pair programming with Robin De Kinders)

--- src/assets/js/playBoard/BuyBuilding.js
+++ src/assets/js/playBoard/BuyBuilding.js
@@ -2,8 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', init);
 
+let selectedCards;
+
 function init(){
-    
+    selectedCards = [];
 }
 function buyBuilding(e){
 
@@ -18,20 +20,49 @@ function buyBuilding(e){
 }
 
 function selectCard(e){
-    let selectedCard = (e.target.closest('div'));
+    let selectedCardHTML = (e.target.closest('div'));
 
     fetchFromServer(`${config.root}games/${localStorage.getItem("gameID")}`, 'GET')
         .then(function (response) {
             let playerCoins = getStartingCoins(response);
+            let selectedCoin = playerCoins[selectedCardHTML.id];
 
-            console.log(playerCoins[selectedCard.id]);
-            if (!selectedCard.classList.contains('selectedCard')){
-                selectedCard.classList.add('selectedCard');
-            }else{
-                selectedCard.classList.remove('selectedCard')
-            }
+            processSelectedCard(selectedCardHTML, selectedCoin);
 
         });
+}
 
+function isIllegalCardSelection(selectedCard){
+    let res = false;
+    selectedCards.forEach(card =>{
+        if (card.currency !== selectedCard.currency){
+            res =  true;
+        }
+    });
+    return false;
+}
+function findCoinIndex(selectedCoin){
+    let i = 0;
+    let index = 0;
+    selectedCards.forEach(coin =>{
+        if (coin.currency === selectedCoin.currency && coin.amount === selectedCoin.amount){
+            index = i;
+        }
+        i++;
+    });
+    return index;
+}
+
+function processSelectedCard(selectedCardHTML, selectedCoin){
+
+    if (!selectedCardHTML.classList.contains('selectedCard')){
+        if (!isIllegalCardSelection(selectedCoin)){
+            selectedCardHTML.classList.add('selectedCard');
+            selectedCards.push(selectedCoin)
+        }
+    }else{
+        selectedCardHTML.classList.remove('selectedCard');
+        selectedCards.splice(findCoinIndex(selectedCoin), 1);
+    }
 
 }
