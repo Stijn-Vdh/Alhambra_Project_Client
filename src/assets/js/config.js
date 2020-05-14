434f4ea6de229d50b01c41d3c7da8b835ac23879
cirdan.de.smedt <cirdan.de.smedt@student.howest.ne>
2020-05-14 11:22:52 +0200

    changed link from school api to own api link

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,6 +2,6 @@
 // Feel free to extend this object with further configurables for your application
 
 const config = {
-    root: "http://172.21.22.52:48201/alhambra-api/",
+    root: "http://172.21.22.52:48201/alhambra-01/api/",
     groupnumber: "01"
 };
