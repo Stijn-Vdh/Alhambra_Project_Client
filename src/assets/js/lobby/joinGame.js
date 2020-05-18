7ffa6f230d5f8c813a767ba0504fed3bd0256c6b
Daniel Vlaeminck <daniel.vlaeminck@student.howest.be>
2020-05-18 14:12:35 +0200

    store encrypted token in lokalstorage replaced fetch code with joinGame function call

--- src/assets/js/lobby/joinGame.js
+++ src/assets/js/lobby/joinGame.js
@@ -1,9 +1,15 @@
 "use strict";
 
-
 function joinGame(gameId,playerName){
     fetchFromServer(`${config.root}games/${gameId}/players`,'POST',{playerName: `${playerName}` })
         .then(function(response){
             console.log('%c%s','background-color: yellow;color: black','The playerToken is ' , response);
+            saveToken(response);
+
         });
+}
+
+function saveToken(response){
+    const token = btoa(response);
+    localStorage.setItem("token", token);
 }
