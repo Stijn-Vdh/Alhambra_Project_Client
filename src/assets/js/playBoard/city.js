add2f8d262cd9c580893fe90835416b9652e9edd
stijn <stijn.van.der.herten@student.howest.be>
2020-05-24 14:30:43 +0200

    refactored city.js

--- src/assets/js/playBoard/city.js
+++ src/assets/js/playBoard/city.js
@@ -8,39 +8,34 @@ function loadCity(){
     let sizeBoard = 7;
     city.innerHTML = "";
 
-    for(let r = 0; r < sizeBoard; r++){
-        for (let  c= 0; c < sizeBoard; c++){
-            let html = `<div class="plateBuilding innerBuildingCity vak${r}${c}"></div>`;
-
+    for(let row = 0; row < sizeBoard; row++){
+        for (let col = 0; col < sizeBoard; col++){
+            let html = `<div class="plateBuilding innerBuildingCity vak${row}${col}"></div>`;
             city.innerHTML += html;
         }
     }
-
 
     fetchFromServer(`${config.root}games/${getGameID()}`,
         'GET')
         .then(function (response) {
 
-            for (let i = 0;i<response['players'].length;i++){
+            for (let i = 0; i < response['players'].length; i++){
                 if (response['players'][i]['name'] === getPlayerName()){
                     city = response['players'][i]["city"]["board"];
-                    for (let i = 0; i<city.length; i++){
-                        for (let j = 0; j < city[i].length;j++){
-                            if (city[i][j] != null){
-                                let building = city[i][j];
-                                let html = document.querySelector(`.vak${i+((sizeBoard-city.length)/2)}${j+((sizeBoard-city.length)/2)}`);
+                    for (let row = 0; row < city.length; row++){
+                        for (let col = 0; col < city[row].length; col++){
+                            if (city[row][col] != null){
+                                let building = city[row][col];
+                                let html = document.querySelector(`.vak${row+((sizeBoard-city.length)/2)}${col+((sizeBoard-city.length)/2)}`);
                                 html.setAttribute('id', `${building['type']}`);
                                 html = loadWalls(html, building);
                             }
                         }
                     }
-
-                    }
                 }
+            }
         });
-
     loadReserve();
-
 }
 
 function placeBuildingInCity(e) {
@@ -56,7 +51,6 @@ function placeBuildingInCity(e) {
             for (let i = 0;i<response['players'].length;i++){
                 if (response['players'][i]['name'] === getPlayerName()){
                     let building = response['players'][i]["buildingsInHand"][0];
-                    selectedSpaceHTML.setAttribute('id', `${building["type"]}`);
                     fetchFromServer(`${config.root}games/${getGameID()}/players/${getPlayerName()}/city`,
                         'POST',
                         {
@@ -69,6 +63,7 @@ function placeBuildingInCity(e) {
                         .then(function () {
                             loadCity();
                             getGameDetails();
+                            selectedSpaceHTML.setAttribute('id', `${building["type"]}`);
                             document.querySelector(".ownPlayerReserveButton").removeEventListener('click', placeBuildingInReserve);
                             document.querySelector(".ownPlayerReserveButton").classList.remove('highlight');
                             document.querySelector("#PlayBoard").removeEventListener('click', placeBuildingInCity);
@@ -77,9 +72,9 @@ function placeBuildingInCity(e) {
             }
         });
 }
+
 function loadWalls(html, building){
     let walls = building['walls'];
-
     if (walls['north']){
         html.classList.add('north');
     }
