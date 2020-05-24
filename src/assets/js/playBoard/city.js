f5b8cd1eaa19bf33f305cd926c0ff834dabd3d0a
robin <robin.de.kinders@student.howest.be>
2020-05-24 20:49:05 +0200

    fixed code smells in city.js

--- src/assets/js/playBoard/city.js
+++ src/assets/js/playBoard/city.js
@@ -1,56 +1,62 @@
 "use strict";
 
-let city = [];
-
-function loadCity(){
+function loadCity() {
 
     let city = document.querySelector("#PlayBoard");
-    let sizeBoard = 7;
+    const sizeBoard = 7;
     city.innerHTML = "";
 
-    for(let row = 0; row < sizeBoard; row++){
-        for (let col = 0; col < sizeBoard; col++){
-            let html = `<div class="plateBuilding innerBuildingCity vak${row}${col}"></div>`;
-            city.innerHTML += html;
-        }
-    }
+    generateGridHtml(city, sizeBoard);
 
     fetchFromServer(`${config.root}games/${getGameID()}`,
         'GET')
         .then(function (response) {
 
-            for (let i = 0; i < response['players'].length; i++){
-                if (response['players'][i]['name'] === getPlayerName()){
+            for (let i = 0; i < response['players'].length; i++) {
+                if (response['players'][i]['name'] === getPlayerName()) {
                     city = response['players'][i]["city"]["board"];
-                    for (let row = 0; row < city.length; row++){
-                        for (let col = 0; col < city[row].length; col++){
-                            if (city[row][col] != null){
-                                let building = city[row][col];
-                                let html = document.querySelector(`.vak${row+((sizeBoard-city.length)/2)}${col+((sizeBoard-city.length)/2)}`);
-                                html.setAttribute('id', `${building['type']}`);
-                                html = loadWalls(html, building);
-                            }
-                        }
-                    }
+                    loadOwnAlhambra(city, sizeBoard);
                 }
             }
         });
     loadReserve();
 }
 
+function loadOwnAlhambra(city, sizeBoard) {
+    for (let row = 0; row < city.length; row++) {
+        for (let col = 0; col < city[row].length; col++) {
+            if (city[row][col] != null) {
+                const building = city[row][col];
+                let html = document.querySelector(`.vak${row + ((sizeBoard - city.length) / 2)}${col + ((sizeBoard - city.length) / 2)}`);
+                html.setAttribute('id', `${building['type']}`);
+                loadWalls(html, building);
+            }
+        }
+    }
+}
+
+function generateGridHtml(city, sizeBoard) {
+    for (let row = 0; row < sizeBoard; row++) {
+        for (let col = 0; col < sizeBoard; col++) {
+            const html = `<div class="plateBuilding innerBuildingCity vak${row}${col}"></div>`;
+            city.innerHTML += html;
+        }
+    }
+}
+
 function placeBuildingInCity(e) {
 
-    let selectedSpaceHTML = (e.target.closest('div'));
+    const selectedSpaceHTML = (e.target.closest('div'));
 
-    let row = selectedSpaceHTML.classList[2].charAt(3)-3;
-    let col = selectedSpaceHTML.classList[2].charAt(4)-3;
+    const row = selectedSpaceHTML.classList[2].charAt(3) - 3;
+    const col = selectedSpaceHTML.classList[2].charAt(4) - 3;
 
     fetchFromServer(`${config.root}games/${getGameID()}`,
         'GET')
         .then(function (response) {
-            for (let i = 0;i<response['players'].length;i++){
-                if (response['players'][i]['name'] === getPlayerName()){
-                    let building = response['players'][i]["buildingsInHand"][0];
+            for (let i = 0; i < response['players'].length; i++) {
+                if (response['players'][i]['name'] === getPlayerName()) {
+                    const building = response['players'][i]["buildingsInHand"][0];
                     fetchFromServer(`${config.root}games/${getGameID()}/players/${getPlayerName()}/city`,
                         'POST',
                         {
@@ -73,18 +79,18 @@ function placeBuildingInCity(e) {
         });
 }
 
-function loadWalls(html, building){
-    let walls = building['walls'];
-    if (walls['north']){
+function loadWalls(html, building) {
+    const walls = building['walls'];
+    if (walls['north']) {
         html.classList.add('north');
     }
-    if (walls['south']){
+    if (walls['south']) {
         html.classList.add('south');
     }
-    if (walls['east']){
+    if (walls['east']) {
         html.classList.add('east');
     }
-    if (walls['west']){
+    if (walls['west']) {
         html.classList.add('west');
     }
     return html;
