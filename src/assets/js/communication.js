6b5a3d23ec55bc34590d12e52bbeea041e7053e1
robin <robin.de.kinders@student.howest.be>
2020-05-24 20:20:39 +0200

    fixed code smells in communication.js

--- src/assets/js/communication.js
+++ src/assets/js/communication.js
@@ -1,44 +1,46 @@
 "use strict";
 
 function fetchFromServer(url, httpVerb, requestBody) {
+    const errorColor = 'background-color: red;color: white';
     const options = {
-    method: httpVerb,
+        method: httpVerb,
 
         headers: {
-        ["Content-Type"]: "application/json",
-        ["Authorization"]: `Bearer ${atob(localStorage.getItem("token"))}`,
-},
-    // Don't forget to add data to the body when needed
-    body: JSON.stringify(requestBody)
-};
+            ["Content-Type"]: "application/json",
+            ["Authorization"]: `Bearer ${atob(localStorage.getItem("token"))}`,
+        },
+        // Don't forget to add data to the body when needed
+        body: JSON.stringify(requestBody)
+    };
 
     return fetch(url, options)
         .then((response) => {
 
             switch (response.status) {
                 case 401:
-                    console.error('%c%s', 'background-color: red;color: white', "You don't have the correct authentication");
+                    console.error('%c%s', errorColor, "You don't have the correct authentication");
                     console.table(response);
                     break;
                 case 403:
-                    console.error('%c%s', 'background-color: red;color: white', 'U have performed a forbidden action');
+                    console.error('%c%s', errorColor, 'U have performed a forbidden action');
                     console.table(response);
                     break;
                 case 409:
-                    console.error('%c%s', 'background-color: red;color: white', "This action is not allowed because it's against the rules");
+                    console.error('%c%s', errorColor, "This action is not allowed because it's against the rules");
                     console.table(response);
                     break;
                 case 422:
-                    console.error('%c%s', 'background-color: red;color: white', 'Player or game does not exist');
+                    console.error('%c%s', errorColor, 'Player or game does not exist');
                     console.table(response);
                     break;
                 case 200:
                     return response.json();
+                default:
+                    return null;
             }
-
-
-    })
-    .then((jsonresponseyouarelookingfor) => {
-        return jsonresponseyouarelookingfor;
-    });
+            return null
+        })
+        .then((jsonResponseYouAreLookingFor) => {
+            return jsonResponseYouAreLookingFor;
+        });
 }
